5fb2cc445fed6804b8918ff11a090b898c5e4b64
gerrywu1990 <gerrywu1990@gmail.com>
2019-08-23 14:11:30 -0400

    clean up unrelated code from create-react app and created new-poll-page component with Styled component and tachyons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,33 +1,17 @@
-import React, { useEffect } from "react";
-import logo from "../../logo.svg";
-import "./index.css";
-import { request } from "../../backend-request";
+import React, { useEffect } from 'react'
+import './style.js'
+import { request } from '../../backend-request'
+
+import NewPollPage from '../new-poll-page'
 
 function App() {
   useEffect(() => {
     const fetchData = async () => {
-      return await request("health");
-    };
-    fetchData();
-  });
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      return await request('health')
+    }
+    fetchData()
+  })
+  return <NewPollPage />
 }
 
-export default App;
+export default App
---
+++ src/components/new-poll-page/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { NewPoll } from './style'
+
+const NewPollPage = () => <NewPoll>Here is the new pull page</NewPoll>
+
+export default NewPollPage
