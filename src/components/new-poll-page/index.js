c01bef4393c4e0f7de6dfa48a12c002d6b0c1902
tunimosmann <rossimosmann@gmail.com>
2019-08-27 19:12:57 -0400

    removed a line on new-poll-page that's throwing an error

--- src/components/new-poll-page/index.js
+++ src/components/new-poll-page/index.js
@@ -4,6 +4,5 @@ import Form from './../Form';
 // import { NewPoll } from './style'
 
 const NewPollPage = () => <div><h1>Here is the new pull page</h1><Form></Form></div> 
-{/* <NewPoll>Here is the new pull page</NewPoll> */}
 
 export default NewPollPage;
