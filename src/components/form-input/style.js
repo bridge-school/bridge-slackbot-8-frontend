0a17199957e83fbcb543099c5950e0d15f595f78
gerrywu1990 <gerrywu1990@gmail.com>
2019-08-27 19:26:43 -0400

    created InputForm and InputError components

---
+++ src/components/form-input/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const InputGroup = styled.div.attrs({
+  className: `mb4`,
+})``
+
+export const Input = styled.input.attrs(props => ({
+  className: `input-reset ba br2 bw1  pa2 mb2 db w-100 f4 ${
+    props.hasError ? 'b--dark-red' : 'b--black-20'
+  }`,
+}))``
+
+export const Label = styled.label.attrs({
+  className: `f4 b db mb2`,
+})``
---
+++ src/components/input-error/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ErrorMessage } from './style'
+
+const InputError = ({ errorMessage, errorFor }) => (
+  <ErrorMessage id={`${errorFor}-error`}>{errorMessage}</ErrorMessage>
+)
+
+export default InputError
