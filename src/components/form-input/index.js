29ef088682a10342c430a7c9e4cc361279ffa61b
Gerry Wu <gerrywu1990@gmail.com>
2019-09-01 16:27:42 -0400

    added validation for Question input and submit handler to form component (#14)
    Thanks!

--- src/components/form-input/index.js
+++ src/components/form-input/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import InputError from '../input-error'
-import { InputGroup, Input, Label } from './style'
 
-const FormInput = ({ id, label, hasError, errorMessage, ...props }) => (
-  <InputGroup>
+import { Input, Label } from './style'
+
+const FormInput = ({ id, label, ...props }) => (
+  <>
     <Label htmlFor={id}>{label}</Label>
     <Input id={id} {...props} />
-    {errorMessage && <InputError errorMessage={errorMessage} errorFor={id} />}
-  </InputGroup>
+  </>
 )
 
 export default FormInput
