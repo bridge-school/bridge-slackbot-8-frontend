668280a449831deee3d257ce7e0b95a338b013cb
gerrywu1990 <gerrywu1990@gmail.com>
2019-09-01 13:16:56 -0400

    added validation for Question input and submit handler to form component

--- src/components/form-input/index.test.js
+++ src/components/form-input/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme, { shallow, mount } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import FormInput from './index'
 
@@ -12,19 +12,5 @@ describe('FormInput component', () => {
     )
 
     expect(wrapper.exists()).toBe(true)
-  })
-
-  test('renders with error message', () => {
-    const id = 'input'
-    const errorMessage = 'some error'
-    const wrapper = mount(
-      <FormInput
-        id={id}
-        label="label for the input"
-        hasError
-        errorMessage={errorMessage}
-      />
-    )
-    expect(wrapper.exists(`#${id}-error`)).toBe(true)
   })
 })
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,40 +1,80 @@
 import React, { Component } from 'react'
+
 import FormInput from '../form-input'
 import FormButton from '../button'
+import InputError from '../input-error'
 import { Form, Legend, Fieldset } from './style'
+
+const validateForm = errors => {
+  let valid = true
+  Object.values(errors).forEach(val => {
+    val.length > 0 && (valid = false)
+  })
+  return valid
+}
 
 class PollForm extends Component {
   constructor() {
     super()
+
     this.state = {
-      question: ''
+      question: '',
+      errors: {
+        question: ''
+      }
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInputChange(event) {
+    const { value } = event.target
+
+    this.setState({
+      question: value,
+      errors: { ...this.state.errors, question: '' }
+    })
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault()
+
+    const { question, errors } = this.state
+
+    await (question.length === 0 &&
+      this.setState({
+        errors: { ...errors, question: 'Question is required' }
+      }))
+
+    try {
+      validateForm(this.state.errors)
+        ? console.log('form valid')
+        : console.log('form invalid')
+      // clear the field on submit
+      this.setState({ question: '' })
+    } catch (error) {
+      console.log(error)
     }
   }
 
-  handleInputChange = event => {
-    this.setState({ question: event.target.value })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-
-    // clear the field on submit
-    this.setState({ question: '' })
-  }
-
   render() {
+    const { question, errors } = this.state
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit} noValidate>
         <Fieldset>
           <Legend>Create New Poll</Legend>
           <FormInput
             id="question"
             name="question"
             label="Question"
-            value={this.state.question}
+            value={question}
             onChange={this.handleInputChange}
             required
           />
+          {errors.question.length > 0 && (
+            <InputError errorMessage={errors.question} />
+          )}
           <FormButton type="submit" onClick={this.handleSubmit}>
             Submit Poll
           </FormButton>
