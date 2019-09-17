1675c529b08b5bbdf80545d19a24e8368514b677
Gerry Wu <gerrywu1990@gmail.com>
2019-09-17 10:28:07 -0400

    601/create poll result page (#27)
    * created poll-result-page, fetch data from backend using mock poll id
    
    * remove pieChart from new-poll-page
    
    * update  piechart styling and created dynamic route
    
    * add loading state and loading spinner
    
    * update route to excatly match
    
    * update route to match links in  submittedPollPage

--- src/components/new-poll-page/index.js
+++ src/components/new-poll-page/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import PollForm from './../form'
-import PieChart from '../pie-chart'
 import { NewPoll } from './style'
 
 const NewPollPage = () => (
   <NewPoll>
     <PollForm />
-    <PieChart />
   </NewPoll>
 )
 
--- src/components/pie-chart/index.js
+++ src/components/pie-chart/index.js
@@ -1,45 +1,6 @@
 import React from 'react'
 import { PieChart, Pie, Tooltip, Legend, Cell } from 'recharts'
 
-const mockData = [
-  {
-    name: 'Yes',
-    value: 10
-  },
-  {
-    name: 'No',
-    value: 2
-  },
-  {
-    name: 'Maybe',
-    value: 4
-  },
-  {
-    name: 'a',
-    value: 10
-  },
-  {
-    name: 'b',
-    value: 2
-  },
-  {
-    name: 'c',
-    value: 4
-  },
-  {
-    name: 'd',
-    value: 10
-  },
-  {
-    name: 'e',
-    value: 2
-  },
-  {
-    name: 'f',
-    value: 4
-  }
-]
-
 const COLORS = [
   '#0088FE',
   '#00C49F',
@@ -54,27 +15,29 @@ const COLORS = [
 ]
 
 const ResultPieChart = ({ data }) => (
-  <PieChart width={850} height={400}>
+  <PieChart width={900} height={500}>
     <Tooltip />
     <Legend
       algin="right"
       verticalAlign="middle"
       layout="vertical"
-      height={36}
+      height={200}
+      iconSize={20}
     />
     <Pie
-      data={mockData}
+      data={data}
       dataKey="value"
       nameKey="name"
       cx="20%"
       cy="50%"
-      innerRadius={100}
-      outerRadius={150}
+      innerRadius={130}
+      outerRadius={180}
       fill="#82ca9d"
     >
-      {mockData.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-      ))}
+      {data &&
+        data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
     </Pie>
   </PieChart>
 )
