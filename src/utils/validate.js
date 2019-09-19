bc2c9e73066874a4afb8f3a02643afbf120e84b1
Florence Shelley <florence@serindlabs.com>
2019-09-19 02:01:20 -0400

    Add comment to describe nullCheck method

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -7,6 +7,7 @@ export const validateForm = errors => {
 }
 
 export const nullCheck = (t, fields) =>
+  // Check if field is empty, and add corresponding error message if empty
   Object.entries(fields).reduce((acc, [key, value]) => {
     const capitalizedKey = key.slice(0, 1).toUpperCase() + key.slice(1)
     return !value ? { ...acc, [key]: t(`${capitalizedKey} is required`) } : acc
